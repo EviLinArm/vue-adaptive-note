1f9168c554f703b71038549cc1a37f3a300158e8
EviLinArm <zakharenko07@gmail.com>
2023-02-08 20:32:09 +1000

    Pages stylised

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,13 +1,36 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-menu">
+    <div class="container is-max-desktop px-2">
       <div class="navbar-brand">
         <RouterLink to="/" class="navbar-item is-size-4 is-bold">Adaptive note</RouterLink>
+        <a @click.prevent="showMobileNav = !showMobileNav" role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" :class="{ 'is-active' : showMobileNav }">
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
       </div>
-      <div class="navbar-end">
-        <RouterLink to="/" class="navbar-item" active-class="is-active">Notes</RouterLink>
-        <RouterLink to="/stats" class="navbar-item" active-class="is-active">Stats</RouterLink>
+      <div class="navbar-menu" :class="{ 'is-active' : showMobileNav }" id="navbarBasicExample">
+        <div class="navbar-end">
+          <RouterLink to="/" class="navbar-item" active-class="is-active">Notes</RouterLink>
+          <RouterLink to="/stats" class="navbar-item" active-class="is-active">Stats</RouterLink>
+        </div>
       </div>
     </div>
   </nav>
 </template>
+
+<script setup>
+import {ref} from "vue";
+
+const showMobileNav = ref(false)
+</script>
+
+<style>
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Write your text here..."/>
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">Add note</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mb-4" v-for="i of 3">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium alias aspernatur blanditiis corporis dolores est excepturi fugit iste laborum odio quae quam recusandae, reprehenderit rerum, sequi veniam veritatis voluptas.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
